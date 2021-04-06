9f2e17fa5e741aaff49b9ec12a084e08a3dcbee0
Jonathan <jonathrakoto91400@gmail.com>
2021-04-06 09:49:33 +0200

    Correction Exo 5

--- lib/exercices/exo5.dart
+++ lib/exercices/exo5.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CarouselImages extends StatelessWidget {
+class CarouselImages extends StatefulWidget {
   CarouselImages({
     Key key,
   }) : super(key: key);
 
+  @override
+  _CarouselImagesState createState() => _CarouselImagesState();
+}
+
+class _CarouselImagesState extends State<CarouselImages> {
+  int _currentIndex = 0;
+
   final List<String> _images = <String>[
-    'https://unsplash.com/photos/ubSUwrr04Ks',
-    'https://unsplash.com/photos/Y84-eo8drzk',
-    'https://unsplash.com/photos/1KPfcPdbWFM',
+    'https://lepetitjournal.com/sites/default/files/styles/main_article/public/2018-08/iStock-615398376.jpg?itok=1k6w3RiH',
+    'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/3b/51/37.jpg',
+    'https://www.nycgo.com/images/pages/71616/zoom-manhattan-bridge-brooklyn-nyc-3000x2000__large.jpg',
+    'https://www.nycgo.com/images/pages/71299/river-skyline-views-brooklyn-nyc-photo-julienne-schaer-010__large.jpg'
   ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Padding(
-      padding: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(15.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Text('Welcome',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
           SizedBox(height: 15),
           Expanded(
             child: ListView.builder(
+              padding: EdgeInsets.zero,
               scrollDirection: Axis.horizontal,
               shrinkWrap: true,
               itemCount: _images.length,
@@ -30,13 +40,32 @@ class CarouselImages extends StatelessWidget {
                 return Padding(
                   padding: EdgeInsets.all(8.0),
                   child: GestureDetector(
-                    onTap: () => print(_images[index]),
-                    child: Text('Image $index')
-                  ),
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                      child: Text('Image ${index + 1}',
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: _currentIndex == index
+                                  ? Colors.black
+                                  : Colors.grey))),
                 );
               },
             ),
-          )
+          ),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(_images[_currentIndex]),
+                fit: BoxFit.cover
+              ),
+              borderRadius: BorderRadius.circular(10)
+            ),
+              height: 400,
+              width: 175,),
         ],
       ),
     ));
