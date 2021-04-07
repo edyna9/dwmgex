357d04aecb96dc73a275e1d4b49f3f51e3fb4118
Jonathan <jonathrakoto91400@gmail.com>
2021-04-07 12:10:37 +0200

    Add Firebase and LoadingScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:dwmgex/routes.dart';
+import 'package:dwmgex/screens/loading.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      routes: routes,
-      initialRoute: '/home',
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Cas erreur de chargement
+        if (snapshot.hasError) {
+          return Container();
+        }
+
+        // Cas chargement réussi
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.indigo,
+            ),
+            routes: routes,
+            initialRoute: '/home',
+          );
+        }
+
+        // Cas de chargement
+        return LoadingScreen();
+      },
     );
   }
 }
